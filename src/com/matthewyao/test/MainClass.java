8f2aefb3bc64206eff084346d068310cecd26b67
姚宽 <mosmovon@yaokuandeMacBook-Air.local>
2016-01-30 01:14:22 +0800

    add thread pool

--- src/com/matthewyao/test/MainClass.java
+++ src/com/matthewyao/test/MainClass.java
@@ -1,15 +1,21 @@
 package com.matthewyao.test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by matthewyao on 2016/1/29.
  */
 public class MainClass {
     public static void main(String[] args) {
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+
         for (int i=0;i<10;i++){
-            new TestMultipleThread().run();
+            pool.execute(new TestMultipleThread());
         }
-//        TestMultipleThread thread = new TestMultipleThread();
-//        thread.run();
+        pool.shutdown();
+        TestMultipleThread thread = new TestMultipleThread();
+        thread.run();
 //        TestJavaURL.curl("http://www.baidu.com");
     }
 }
