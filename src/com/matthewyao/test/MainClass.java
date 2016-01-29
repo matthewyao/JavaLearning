d026cf2ab32fe2916463a6b2279155f0464ab465
matthewyao <mosmovon@gmail.com>
2016-01-29 15:24:06 +0800

    add java url and save as file

---
+++ src/com/matthewyao/test/MainClass.java
@@ -0,0 +1,10 @@
+package com.matthewyao.test;
+
+/**
+ * Created by matthewyao on 2016/1/29.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        TestJavaURL.curl("http://www.baidu.com");
+    }
+}
