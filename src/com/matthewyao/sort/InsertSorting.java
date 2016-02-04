c0abdb308cee6c308c52851ec720bf0d9cdd1b05
姚宽 <mosmovon@yaokuandeMacBook-Air.local>
2016-02-04 18:02:13 +0800

    first commit

---
+++ src/com/matthewyao/sort/InsertSorting.java
@@ -0,0 +1,13 @@
+package com.matthewyao.sort;
+
+/**
+ * Created by mosmovon on 16/2/4.
+ */
+public class InsertSorting {
+    public int InsertSort(int[] array){
+        for (int i=1;i<array.length;i++){
+            int temp = array[i];
+            for (int j = i - 1;j >= 0 && temp<=array[i])
+        }
+    }
+}
---
+++ src/com/matthewyao/sort/MainClass.java
@@ -0,0 +1,10 @@
+package com.matthewyao.sort;
+
+/**
+ * Created by mosmovon on 16/2/4.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
