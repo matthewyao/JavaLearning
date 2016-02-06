087a577a1c8eee49bcb57c157ac750f533210624
姚宽 <mosmovon@gmail.com>
2016-02-06 22:17:39 +0800

    insist

--- src/com/matthewyao/sort/MainClass.java
+++ src/com/matthewyao/sort/MainClass.java
@@ -4,6 +4,7 @@ package com.matthewyao.sort;
  * Created by mosmovon on 16/2/4.
  */
 public class MainClass {
+    //for sorting
     public static void main(String[] args) {
         int[] initArray = {3,2,7,1,8,3,9,2,54,24,67,34,22},sortArray;
         System.out.print("Before sort:");
