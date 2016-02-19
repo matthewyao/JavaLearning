1928aa752624181126f89edeca9614a170ed7b9c
matthewyao <mosmovon@gmail.com>
2016-02-19 11:53:31 +0800

    finish

--- src/com/matthew/base64decode/Test.java
+++ src/com/matthew/base64decode/Test.java
@@ -1,6 +1,9 @@
 package com.matthew.base64decode;
 
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URLDecoder;
 
 import com.allyes.carpenter.CarpenterLogs;
 
@@ -8,17 +11,61 @@ import sun.misc.BASE64Decoder;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "CIm8uLQFEAQaA2lmYyAAKhNDbGt0cjJ1a04wQlRleGIxNHREMgsyNy4xOTIuOC42MTrGAmh0dHA6Ly9pZGlnZ2VyLnF0bW9qby5jb20vbWFpbi9hZGZ0cmFjaz9sdD1pJmRiPWlmYyZhbz1ULTAwMDEzMC0wMSZ0YWc9JmVjPSZlYT0mZWw9JmV2PSZvYz0mc2U9Jmt3PSZjdGQ9JmFjdj1jb252dHlwZSU3Qztjb252YWN0aW9uJTdDO2NvbnZpZCU3Qztjb252dmFsdWUlN0MmZWNtPTYwNDUxNTMwMDAlNjAlNjAlNjAlNjAlNjAlNjAlNjAlNjAlNjAlNjAlNjAlNjAlNjAmdGt2PSZjY3Q9MTQ1MDk3MjkzNCZzYz00Jm52PTAmaG49Yy5tLnN1bmluZy5jb20mcHU9JTJGY2hhbm5lbCUyRm5pYW5odW8uaHRtbCZyZj0mZ3BhPWlqM3o4MDI2YjM1NXQmcj0xNDUyMTU0Mzc2NTM1QipodHRwOi8vYy5tLnN1bmluZy5jb20vY2hhbm5lbC9uaWFuaHVvLmh0bWxKpgFNb3ppbGxhLzUuMChMaW51eDsgVTtTTkVCVVktQVBQO1NORUJVWS1BUFAgMTEwO1NOQ0xJRU5ULVdBUDsgQW5kcm9pZCA0LjIuMjsgemg7IEhVQVdFSSBHNzUwLVQwMSkgQXBwbGVXZWJLaXQvNTMzLjAgKEtIVE1MLCBsaWtlIEdlY2tvKSBWZXJzaW9uLzQuMCBNb2JpbGUgU2FmYXJpLzUzMy4xUgx6aC1DTiwgZW4tVVNaAGIAagByATB6AIIBAIgBAJABAJoBATCgAdeMFKoBB2lEaWdnZXKyAQC4AQDCAQDKAQDSAQDYAQDiAQDqAQDyAQD6AQCiBg5kZS41LjIzLmlkLnNqY6oGOS93d3cvYWxseWVzL2RlL2xvZ3MvZGVfbG9nLmlmYy4yMDE2LTAxLTA3LjE2LjEwLjI4LjUuMjMuNLAGzKkG";
-		try {
-			byte[] b = new BASE64Decoder().decodeBuffer(str);
-			System.out.println(b);
-			CarpenterLogs.RawLog rawLog = CarpenterLogs.RawLog.parseFrom(b);
-			System.out.println(rawLog.getAllyesId());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+	public static String TimeStamp2Date(String timestampString, String formats){
+		Long timestamp = Long.parseLong(timestampString)*1000;
+		String date = new java.text.SimpleDateFormat(formats).format(new java.util.Date(timestamp));
+		return date;
+	}
+
+	public static void main(String[] args) throws Exception{
+		FileReader fr;
+		FileWriter fw;
+		BufferedReader br;
+		BufferedWriter bw;
+		int i = 1;
+		String line;
+		try{
+			fr = new FileReader("E:/idigger/lines.log");
+			fw = new FileWriter("E:/idigger/result.txt");
+			br = new BufferedReader(fr);
+			bw = new BufferedWriter(fw);
+			while ((line = br.readLine()) != null){
+				byte[] b = new BASE64Decoder().decodeBuffer(line);
+				CarpenterLogs.RawLog rawLog = CarpenterLogs.RawLog.parseFrom(b);
+				String time = TimeStamp2Date(String.valueOf(rawLog.getTimestamp()),"yyyy-MM-dd HH:mm:ss");
+				String url = rawLog.getRequestUrl();
+				url = url.substring(url.indexOf("ecm=")+4, url.length());
+				url = url.substring(0, url.indexOf("&"));
+				url = URLDecoder.decode(url, "utf-8");
+				url = URLDecoder.decode(url, "utf-8");
+				url = URLDecoder.decode(url, "utf-8");
+				url = url.replaceAll("`","\t");
+				if (url.length()>13 && ! url.startsWith("p")){
+//					System.out.println(time + "\t" + url);
+					bw.write(time + "\t" + url);
+					bw.newLine();
+				}
+				i++;
+
+//				Field[] fields = rawLog.getClass().getFields();
+//				for(Field f : fields){
+//					Method m;
+//					if (f.getName().equals("PARSER")){
+//
+//					}
+//					else{
+//						m = rawLog.getClass().getMethod("get" + f.getName());
+//					}
+//					String val = m.invoke(rawLog).toString();
+//					bw.write(val);
+//				}
+//				bw.write("\n");
+			}
+		}catch (Exception e){
 			e.printStackTrace();
+			System.out.println(e.getMessage()+",i:"+i);
+		}
+		finally {
 		}
 	}
 
