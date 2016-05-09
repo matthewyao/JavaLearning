de45256d5399c0520cc5f4ddfd467f1345e837ec
matthewyao <mosmovon@gmail.com>
2016-05-09 18:03:27 +0800

    first compare diff

---
+++ src/MainClass.java
@@ -0,0 +1,10 @@
+import com.matthewyao.work.AdIesAreaMatch;
+
+/**
+ * Created by matthewyao on 2016/5/9.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        new AdIesAreaMatch().matchAreaName();
+    }
+}
