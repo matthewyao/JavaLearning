bc89e4ea2e0dce4a6fd099b877fea2aa225422d2
matthewyao <mosmovon@gmail.com>
2016-05-30 11:57:27 +0800

    finish

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,6 @@
 import com.matthewyao.sikuli.JavaSikuliDemo;
 import com.matthewyao.sikuli.TestOCR;
+import com.matthewyao.work.AdIESHmacSHATest;
 
 /**
  * Created by matthewyao on 2016/5/9.
@@ -10,8 +11,8 @@ public class MainClass {
 //        new VerifyCodeUtil().execute();//测试验证码
 //        new TestWebDriver().execute();//测试webDriver
 //        new AdIESHmacSHA().execute();//Admaster IES功能
-//        new AdIESHmacSHATest().execute();
+        new AdIESHmacSHATest().execute();
 //        new JavaSikuliDemo().execute();//测试Sikuli拍牌
-        new TestOCR().execute();//测试OCR功能
+//        new TestOCR().execute();//测试OCR功能
     }
 }
