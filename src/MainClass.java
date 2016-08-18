93f50a20ce3ffebd25a7ce53e28f979aa3f92755
matthewyao <mosmovon@gmail.com>
2016-08-18 10:51:20 +0800

    增加一些实用的类

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,9 @@
 import com.matthewyao.sikuli.JavaSikuliDemo;
 import com.matthewyao.sikuli.TestOCR;
+import com.matthewyao.test.TimeTest;
 import com.matthewyao.work.AdIESHmacSHATest;
+
+import java.text.ParseException;
 
 /**
  * Created by matthewyao on 2016/5/9.
@@ -11,8 +14,13 @@ public class MainClass {
 //        new VerifyCodeUtil().execute();//测试验证码
 //        new TestWebDriver().execute();//测试webDriver
 //        new AdIESHmacSHA().execute();//Admaster IES功能
-        new AdIESHmacSHATest().execute();
+//        new AdIESHmacSHATest().execute();
 //        new JavaSikuliDemo().execute();//测试Sikuli拍牌
 //        new TestOCR().execute();//测试OCR功能
+        try {
+            TimeTest.execute();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/com/matthewyao/work/AesDemo.java
@@ -0,0 +1,24 @@
+package com.matthewyao.work;
+
+import org.apache.commons.codec.binary.Hex;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+
+/**
+ * Created by matthewyao on 2016/7/29.
+ */
+public class AesDemo {
+
+    public static void main(String... args){
+        try {
+            String price = "100";
+            String encryptStr = AESEncrypt.getEncryptStr(price,"8f5b054659b4cbb947d07b8facd00f28");
+            System.out.println(encryptStr);
+        } catch (Exception e) {
+            e.printStackTrace() ;
+        }
+    }
+}
