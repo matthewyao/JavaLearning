9381bfb90a027ba644e0758edfee76ff99ac1dcd
matthewyao <mosmovon@gmail.com>
2017-02-05 15:09:25 +0800

    finish

--- src/main/java/com/matthewyao/leetcode/L169_MajorityElement.java
+++ src/main/java/com/matthewyao/leetcode/L169_MajorityElement.java
@@ -1,18 +1,17 @@
 package com.matthewyao.leetcode;
 
+import java.util.Arrays;
+
 /**
  * Created by matthewyao on 2017/1/24.
  */
 public class L169_MajorityElement {
     public static void main(String[] args) {
-
+        System.out.println(majorityElement(new int[]{3,5,6,3,2,3,4,3,3,3,2,1,2,3,4,3,3}));
     }
 
-    public int majorityElement(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int val = nums[i];
-
-        }
-        return 0;
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
